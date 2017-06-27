fa23a02e0175a339c16d25a92a58aaa5f49cf758
Jakub Sochor <isochor@fit.vutbr.cz>
2017-06-27 13:39:17 +0200

    anaconda path

--- bin/init_env.sh
+++ bin/init_env.sh
@@ -45,6 +45,8 @@ else
 	alias qlog-gpu='qlogin -q long.q@@gpu -l gpu=1,matylda1=10,mem_free=5G,ram_free=5G'
 	alias matlab='LD_PRELOAD="/usr/lib64/libstdc++.so.6" matlab'
 	export PYTHONHOME="$PREFIX/anaconda3"
+	export PATH="$PYTHONHOME/bin:$PATH"
+	echo "Enabling python from $PYTHONHOME"
 	export CUDAHOME="/usr/local/share/cuda"
 	export PS1="\[$(tput bold)\]\[$(tput setaf 7)\][\[$(tput setaf 3)\]\t \\[$(tput setaf 2)\]\u\[$(tput setaf 5)\]@\[$(tput setaf 5)\]\h \[$(tput setaf 4)\]\w\[$(tput setaf 7)\]]\[$(tput setaf 4)\] \[$(tput sgr0)\]\n\[$(tput bold)\]\[$(tput setaf 4)\]$ \[$(tput sgr0)\]"	
 fi
